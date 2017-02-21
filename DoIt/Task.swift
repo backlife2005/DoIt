5a105bee10cd9dfe8245099ea1a5ac96ea4301d2
Mohammed Al-Khonaizi <backlife2005@gmail.com>
2017-02-22 02:59:31 +0300

    Added a swift file for the Task class.
    Declared a class name Task with two variables.
    
    Declared a variable named name and set it’s value to “”.
    
    Declared a variable named important and set it’s value to false.

---
+++ DoIt/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Mohammed Al-Khonaizi on 2/22/17.
+//  Copyright © 2017 Mohammed Al-Khonaizi. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var name = ""
+    var important = false
+    
+    
+}
